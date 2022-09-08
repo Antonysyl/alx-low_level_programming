0dba86ecb0c2c5ec6542eef028f5c8271579f653
Antonysyl <onyedikaantony@gmail.com>
2022-09-08 10:19:05 -0700

    c is fun indeed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * I used only printf.
+ * My program returns to 0
+ */
+int main(void)
+{
+printf("with proper grammar but the outcome is a piece of art,\n");
+return (0);
+}
