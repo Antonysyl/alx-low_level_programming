674655bc6203c353dc7b4c27de62afc2784e78fe
Antonysyl <onyedikaantony@gmail.com>
2022-09-08 11:36:37 -0700

    finally beginning to understand c language better

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
+ * main - print quote
  *
- * Return: Always 0 (Success)
+ * Description: prints Dors's quote
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-write(2, "and thatr piece of art work is useful\ - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-return (0);
+write(2, "and that piece of art work is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
