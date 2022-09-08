207c8951a6c9a8261ebbfb209e2b0192efdfa048
Antonysyl <onyedikaantony@gmail.com>
2022-09-08 10:25:18 -0700

    c is fun indeed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * I used only printf.
- * My program returns to 0
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-printf("with proper grammar but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
