28b966e0e5472fde0316852709e65d334a226456
Antonysyl <onyedikaantony@gmail.com>
2022-09-13 11:49:35 -0700

    nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,21 +7,16 @@
  */
 void print_to_98(int n)
 {
-if (n < 98)
+while (n < 98)
 {
-for (; n < 98; n++)
-{
-printf("%d, ", n);
+printf("%i, ", n);
+n++;
 }
-}
-else if (n > 98)
+while (n > 98)
 {
-for(; n > 98; n--)
-{
-printf("%d, ", n);
+printf("%i, ", n);
+n--;
 }
-if (n == 98)
-{
-printf("%d\n", n);
-}
+printf("98");
+putchar('\n');
 }
