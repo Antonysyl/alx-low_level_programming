1ea35489059c2b280711e33120e1896d65918bf7
Antonysyl <onyedikaantony@gmail.com>
2022-09-13 07:07:00 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
-void jack_bauer(void)
+void jack_bauer(void);
 int add(int a, int b);
 void times_table(void);
 void print_to_98(int n);
