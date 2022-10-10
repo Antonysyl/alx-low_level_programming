5969ec97542dfa2100c886ed1d9c2b40ac252365
Antonysyl <onyedikaantony@gmail.com>
2022-10-10 12:11:17 -0700

    structures and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,6 @@ printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
-else										printf("Owner: %s\n", d->owner);
+else
+printf("Owner: %s\n", d->owner);
 }
