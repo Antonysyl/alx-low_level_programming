49bc9fcabfdfa7f9ae5fbae08aa8d385248fa4e0
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-10-27 17:46:24 +0100

    converting a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,57 +1,30 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 /**
- * _pow_recursion - function that returns the value of x
- * raised to the power of y
- * @x: base number
- * @y: pow number
- * Return: int
- */
-int _pow_recursion(int x, int y)
-{
-if (y < 0)
-return (-1);
-if (y == 0)
-return (1);
-return (x * _pow_recursion(x, y - 1));
-}
-/**
- * _strlen - function that returns the length of a string.
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * @s: pointer to an string
- * Return: int
- */
-int _strlen(const char *s)
-{
-int i = 0;
-while (s[i] != '\0')
-{
-i += 1;
-}
-return (i);
-}
-/**
- * binary_to_uint - function that converts a binary number to an unsigned int
- * @b: pointing to a string of 0 and 1 chars
- * Return: the converted number, or 0 if
- * there is one or more chars in the string b that is not 0 or 1
- * b is NULL
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-int len, exp = 0;
-unsigned int res = 0;
-if (b == NULL)
+unsigned int ui;
+int len, base_two;
+if (!b)
 return (0);
-len = _strlen(b);
-while (len-- && len >= 0)
+ui = 0;
+for (len = 0; b[len] != '\0'; len++)
+;
+for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 {
-if (b[len] == '1')
-res += _pow_recursion(2, exp);
-else if (b[len] != '0')
+if (b[len] != '0' && b[len] != '1')
+{
 return (0);
-exp++;
 }
-return (res);
+if (b[len] & 1)
+{
+ui += base_two;
+}
+}
+return (ui);
 }
