9c06283f251129f003e9731788890edb6f676714
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-10-27 17:43:20 +0100

    header

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ */
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -8,4 +14,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-#endif /* #ifndef MAIN_H */
+
+#endif /* MAIN_H */
