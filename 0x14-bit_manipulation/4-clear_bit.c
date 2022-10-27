867f9f5900ac995f42c6fcde4712050a22892f55
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-10-27 18:01:10 +0100

    setting the value of a bit to a given index at 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,25 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * clear_bit - sets the bit at a given index to 0
- * @n: pointer to the number
- * @index: index to set
- * Return: 1 if it worked, -1 if an error occured
+ * clear_bit - Sets the value of a bit at a given index to 0.
+ * @n: A pointer to the bit.
+ * @index: The index to set the value at - indices start at 0.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - 1.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int mask;
-unsigned int s_index;
-s_index = 0;
-mask = 1;
-while (mask < 2147483648)
-{
-if (s_index == index)
-{
-*n = *n & ~(1 << index);
-return (1);
-}
-s_index++;
-mask = mask << 1;
-}
+if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
+*n &= ~(1 << index);
+return (1);
 }
