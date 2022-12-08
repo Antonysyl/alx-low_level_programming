d47e7de6f57c230558bf00fb11e3e683ca3ea4fc
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-12-08 18:32:47 +0100

    adding a new node at the beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * add_dnodeint - adds a new node at the beginning
  * of a dlistint_t list
@@ -10,29 +9,22 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-	dlistint_t *h;
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->prev = NULL;
-	h = *head;
-
-	if (h != NULL)
-	{
-		while (h->prev != NULL)
-			h = h->prev;
-	}
-
-	new->next = h;
-
-	if (h != NULL)
-		h->prev = new;
-
-	*head = new;
-
-	return (new);
+dlistint_t *new;
+dlistint_t *h;
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->prev = NULL;
+h = *head;
+if (h != NULL)
+{
+while (h->prev != NULL)
+h = h->prev;
+}
+new->next = h;
+if (h != NULL)
+h->prev = new;
+*head = new;
+return (new);
 }
